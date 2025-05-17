ba2648945af0c9336a2fb6e134011fca1507946b
ranji <ranji@192.168.1.9>
2025-05-17 15:08:57 +0530

    Second Commit

---
+++ Project_1/src/firstpackage/PrimeNumber.java
@@ -0,0 +1,32 @@
+package firstpackage;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int fc=0;
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				fc++;
+			}
+		}
+		if(fc==2)
+		{
+			System.out.println(n+" The number is Prime");
+		}
+		else
+		{
+			System.out.println(n+" The Number is Not prime");
+		}
+
+	}
+
+}
