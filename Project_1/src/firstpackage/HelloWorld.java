dc57b5fd98ec16ae57d0a7964101af5603db9399
ranji <ranji@192.168.1.9>
2025-05-17 14:07:15 +0530

    First Commit

---
+++ Project_1/src/firstpackage/HelloWorld.java
@@ -0,0 +1,12 @@
+package firstpackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("My First Program");
+
+	}
+
+}
