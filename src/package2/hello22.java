08449a99bc4d0fafbb40264dfe9e63aabfa118da
ranji <ranji@192.168.1.9>
2025-05-19 13:22:34 +0530

    Merge branch 'master' of https://github.com/Ranji1211/PROJECT_SESSION.git

---
+++ src/package2/hello22.java
@@ -0,0 +1,12 @@
+package package2;
+
+public class hello22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My 2nd project prgram");
+		System.out.println("My 2nd project prgram");
+
+	}
+
+}
